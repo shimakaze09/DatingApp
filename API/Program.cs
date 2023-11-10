ce508dd846e6846930213d0c3a95a9823d85c3e0
John <yangtianzhuo970425@gmail.com>
2023-11-10 15:36:30 +1300

    Adding a DbContext class

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,11 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options => { options.UseSqlite("Connection String"); });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
