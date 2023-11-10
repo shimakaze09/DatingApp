33f2c51a94b36f913fa5ac1cb60a59979853e939
John <yangtianzhuo970425@gmail.com>
2023-11-10 19:40:18 +1300

    Using HTTPS in angular

--- API/Program.cs
+++ API/Program.cs
@@ -20,7 +20,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
-    .WithOrigins("http://localhost:4200"));
+    .WithOrigins("https://localhost:4200"));
 
 if (app.Environment.IsDevelopment())
 {
